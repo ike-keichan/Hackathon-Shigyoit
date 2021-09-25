75a10bca636e9f077a81f02a5b70a0a5ca3e82f8
ike-keichan <ikeike.keikeichan0908@gmail.com>
2021-09-25 16:56:23 +0900

    fix: ファイル類の修正。

---
+++ src/styles/colors.ts
@@ -0,0 +1,5 @@
+export const colors = {
+  COLOR_666666: '#666666',
+  COLOR_61CACE: '#61CACE',
+  COLOR_FFFFFF: '#ffffff',
+} as const
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,3 +1,6 @@
+import React from 'react'
+
+
 html,
 body {
   padding: 0;
