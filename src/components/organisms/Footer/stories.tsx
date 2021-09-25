431caf79cdbf1240aeb037deb9be08dab8d6ba60
Ayamoto Kohei <koh14taka2@gmail.com>
2021-09-25 20:45:51 +0900

    add:フッターコンポーネントの作成

---
+++ src/components/organisms/Footer/stories.tsx
@@ -0,0 +1,10 @@
+import { Footer } from './index'
+
+export default {
+  component: Footer,
+  title: 'atoms/Footer',
+}
+
+const Component= () => <Footer />
+
+export const Base = Component.bind({})
