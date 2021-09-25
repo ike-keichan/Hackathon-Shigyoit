6f377139e8e3ae8dfa788f3b089f145b9e6c8720
Ayamoto Kohei <koh14taka2@gmail.com>
2021-09-26 08:35:49 +0900

    add: ボタンコンポーネントの追加

---
+++ src/components/atoms/Button/stories.tsx
@@ -0,0 +1,16 @@
+import { Story } from "@storybook/react"
+
+import { Button, ButtonProps } from "./index"
+
+export default {
+    component: Button,
+    title: 'atoms/Button',
+}
+
+const Component: Story<ButtonProps> = ({children, ...buttonProps}) => <Button {...buttonProps}>{children}</Button>
+
+export const Base = Component.bind({})
+
+Base.args = {
+    children: "あの人の気分も調べる"
+}
